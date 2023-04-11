960003f0ad2415a549a045837bccd9c3013b735f
Anahi Felix <afelixtolentino@frba.utn.edu.ar>
2023-04-11 00:43:52 -0300

    Tp 0 terminado

--- 00-CHelloWorld/hello.c
+++ 00-CHelloWorld/hello.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-
 int main() {
+  FILE *punteroArchivo = fopen("output.txt","w");
+  fprintf(punteroArchivo, "Hola mundo en txt!");
+  fclose(punteroArchivo);
 
+  printf("HOLA MUNDOOO!!");
+  
   return 0;
 }
